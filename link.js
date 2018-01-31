d4d9af3343da210c7ce07317d23dc10a23a47f3d
Brent Vatne <brentvatne@gmail.com>
2018-01-31 10:01:12 -0800

    Update from super old version of React/React Native and bump version

--- link.js
+++ link.js
@@ -4,37 +4,23 @@
  * Currently links to http:// URLs
  *
  */
-var React = require('react-native');
 
-var {
-  AppRegistry,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-  Linking,
-} = React;
+import React from 'react';
+import { TouchableHighlight, Linking } from 'react-native';
 
-var Link = React.createClass({
+export default class Link extends React.Component {
   render() {
-    return (
-      <TouchableHighlight onPress={this._linkPressed} {...this.props} />
-    );
-  },
+    return <TouchableHighlight onPress={this._linkPressed} {...this.props} />;
+  }
 
-  _linkPressed() {
+  _linkPressed = () => {
     if (this.props.url) {
       Linking.openURL(this.props.url);
     } else if (this.props.to && this.props.to.uri) {
-      var url = this.props.to.uri;
+      const url = this.props.to.uri;
       Linking.openURL(url);
     } else {
-      console.error("Invalid Link destination:", this.props.to);
+      console.error('Invalid Link destination:', this.props.to);
     }
-  },
-
-});
-
-module.exports = Link;
+  };
+}
